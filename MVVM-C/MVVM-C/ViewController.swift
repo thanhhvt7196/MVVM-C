67e02e213b0ec130e412881bfbf12842e98418e6
thanh tien <thanhhvt7196@gmail.com>
2020-07-17 13:20:41 +0700

    first commit

---
+++ MVVM-C/MVVM-C/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM-C
+//
+//  Created by thanh tien on 7/17/20.
+//  Copyright © 2020 thanh tien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
